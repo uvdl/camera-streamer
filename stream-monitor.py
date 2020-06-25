8a3016243c6d0a356fb4ae46578baa80317e110b
Bogdan Vacaliuc <bvacaliuc@gmail.com>
2020-06-24 22:14:53 -0400

    change H264_XXX parameters to VIDEO_XXX

--- stream-monitor.py
+++ stream-monitor.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
 
     dev = sys.argv[1] if len(sys.argv)>1 else os.environ.get('MONITOR_DEV', os.environ.get('UDP_IFACE', 'wlan0'))
     pin = sys.argv[2] if len(sys.argv)>2 else os.environ.get('MONITOR_PIN', '21')
-    kbps = sys.argv[3] if len(sys.argv)>3 else os.environ.get('MONITOR_KBPS', os.environ.get('H264_BITRATE', '1800'))
+    kbps = sys.argv[3] if len(sys.argv)>3 else os.environ.get('MONITOR_KBPS', os.environ.get('VIDEO_BITRATE', '1800'))
     pwm_hz = sys.argv[4] if len(sys.argv)>4 else os.environ.get('MONITOR_PWM_HZ', '100')
     update_sec = sys.argv[5] if len(sys.argv)>5 else os.environ.get('MONITOR_UPDATE_SEC', '1.0')
 
