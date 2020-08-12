2d27814a12388d697cf716a04760a522bec4e18f
Bogdan Vacaliuc <bvacaliuc@gmail.com>
2020-08-12 07:24:28 -0400

    towards multi-camera support

--- Makefile
+++ Makefile
@@ -6,25 +6,49 @@ SUDO := $(shell test $${EUID} -ne 0 && echo "sudo")
 DRY_RUN := $(if $(findstring n,$(firstword -$(MAKEFLAGS))),--dry-run)
 .EXPORT_ALL_VARIABLES:
 
-PKGDEPS=sudo uvcdynctrl v4l-utils python3-netifaces python3-pip
+PKGDEPS=sudo python3-netifaces python3-pip uvcdynctrl v4l-utils
 
 LOCAL=/usr/local
 LOCAL_APPS=gst-client gstd-client gst-client-1.0 gstd speedtest-cli
 LOCAL_SCRIPTS=internet.py video-stream.sh stream-monitor.py
-FLAGS ?= "audio,h264,mjpg,rtmp,xraw"
+FLAGS ?= "h264,mjpg,rtmp,xraw"
 LIBSYSTEMD=/lib/systemd/system
 PLATFORM ?= $(shell python3 serial_number.py | cut -c1-4)
-RIDGERUN=https://github.com/RidgeRun
 SERVER ?= video.mavnet.online
 SERVER_PORT ?= 1935
 SERVER_GROUP ?= live/ORNL
-SERVICES=video-stream.service stream-monitor.service
+SERVICES=camera-switcher.service audio-streamer.service stream-monitor.service
 SIVEL=https://github.com/sivel
 SPEEDTEST=$(LOCAL)/bin/speedtest-cli
 SPEEDTEST_SRC=$(LOCAL)/src/speedtest-cli
-SYSCFG=/etc/systemd/video-stream.conf
+SYSCFG=/etc/systemd
 
-.PHONY = clean dependencies disable enable git-cache install provision test uninstall
+.PHONY = clean dependencies disable enable git-cache install
+.PHONY = provision provision-audio provision-cameras provision-video
+.PHONY = show-config test uninstall
+
+default:
+	@echo "Please choose an action:"
+	@echo ""
+	@echo "  dependencies: ensure all needed software is installed (requires internet)"
+	@echo "  install: update programs and system scripts"
+	@echo "  provision: interactively define the needed configurations (all of them)"
+	@echo ""
+	@echo "The above are issued in the order shown above.  dependencies is only done once."
+	@echo "Once the system is setup, you can use these two shortcuts to modify video parameters:"
+	@echo ""
+	@echo "  provision-audio: re-adjust audio streaming parameters"
+	@echo "  provision-video: re-adjust video streaming parameters"
+	@echo "  provision-cameras: re-assign cameras"
+	@echo ""
+
+$(LOCAL)/bin/audio-streamer.sh:
+	$(SUDO) systemctl stop audio-streamer
+	PLATFORM=$(PLATFORM) ./provision.sh $@ $(DRY_RUN)
+
+$(LOCAL)/bin/camera-switcher.sh:
+	$(SUDO) systemctl stop camera-switcher
+	$(SUDO) install -Dm755 ./camera-switcher.sh $@
 
 $(LOCAL)/src:
 	@if [ ! -d $@ ] ; then mkdir -p $@ ; fi
@@ -36,70 +60,25 @@ $(SPEEDTEST_SRC): $(LOCAL)/src
 $(SPEEDTEST): $(SPEEDTEST_SRC)
 	$(SUDO) install -Dm755 $</speedtest.py $@
 
+$(SYSCFG)/%.conf:
+	PLATFORM=$(PLATFORM) ./provision.sh $@ $(DRY_RUN)
+
 # https://stackoverflow.com/questions/3980668/how-to-get-a-password-from-a-shell-script-without-echoing
 # TODO: figure out use of an encrypted filesystem to hold the configuration file
 # https://www.linuxjournal.com/article/9400
 # FIXME: this shell code in makefile is really, really dumb...
 $(SYSCFG): serial_number.py
-	@(	SN=$(shell python3 serial_number.py) && \
-		URL=$(shell $(SUDO) grep URL $(SYSCFG) | cut -f2 -d=) && \
-		SKEY=$(shell $(SUDO) grep SKEY $(SYSCFG) | cut -f2 -d=) && \
-		AUDIO=$(shell $(SUDO) grep AUDIO $(SYSCFG) | cut -f2 -d=) && \
-		LATENCY_MS=$(shell $(SUDO) grep LATENCY_MS $(SYSCFG) | cut -f2 -d=) && \
-		read -p "udp or URL for video server? ($${URL}) " UR && \
-		if [ ! -z "$${UR}" ] ; then URL=$${UR} ; fi ; \
-		if [ -z "$${SKEY}}" ] ; then SKEY=$${SN} ; fi ; \
-		read -p "Stream Key? ($${SKEY}) " SK && \
-		if [ ! -z "$${SK}" ] ; then SKEY=$${SK} ; fi ; \
-		echo "[Service]" > /tmp/$$.env ; \
-		x=$$(echo $$URL | grep mavnet.online) && \
-		if [ ! -z "$$x" ] ; then \
-			USERNAME=$(shell $(SUDO) grep USERNAME $(SYSCFG) | cut -f2 -d=) && \
-			read -p "Username for video server? ($${USERNAME}) " UN && \
-			if [ ! -z "$${UN}" ] ; then USERNAME=$${UN} ; fi ; \
-			read -s -p "Password? " KEY ; echo "" ; \
-			echo "KEY=$${KEY}" >> /tmp/$$.env && \
-			echo "USERNAME=$${USERNAME}" >> /tmp/$$.env && \
-			echo "SERVER=$${SERVER}" >> /tmp/$$.env && \
-			SKEY="$${SN}" && \
-			URL="rtmp://$${SERVER}:$${SERVER_PORT}/$${SERVER_GROUP}" ; \
-		fi ; \
-		if [ "$$URL" == "udp" ] ; then \
-			FLAGS="audio,h264,mjpg,udp,xraw"
-			UDP_IFACE=$(shell $(SUDO) grep UDP_IFACE $(SYSCFG) | cut -f2 -d=) && \
-			UDP_HOST=$(shell $(SUDO) grep UDP_HOST $(SYSCFG) | cut -f2 -d=) && \
-			UDP_PORT=$(shell $(SUDO) grep UDP_PORT $(SYSCFG) | cut -f2 -d=) && \
-			read -p "Network interface device for udp ($${UDP_IFACE}) " UIF && \
-			if [ ! -z "$${UIF}" ] ; then UDP_IFACE=$${UIF} ; fi ; \
-			read -p "UDP port for video stream? ($${UDP_PORT}) " UP && \
-			if [ ! -z "$${UP}" ] ; then UDP_PORT=$${UP} ; fi ; \
-			read -p "UDP address for video stream? ($${UDP_HOST}) " UH && \
-			if [ ! -z "$${UH}" ] ; then UDP_HOST=$${UH} ; fi ; \
-			echo "UDP_IFACE=$${UDP_IFACE}" >> /tmp/$$.env && \
-			echo "UDP_HOST=$${UDP_HOST}" >> /tmp/$$.env && \
-			echo "UDP_PORT=$${UDP_PORT}" >> /tmp/$$.env ; \
-		fi ; \
-		echo "URL=$${URL}" >> /tmp/$$.env && \
-		echo "SKEY=$${SKEY}" >> /tmp/$$.env && \
-		echo "FLAGS=$(FLAGS)" >> /tmp/$$.env && \
-		read -p "Audio Device? ($${AUDIO}) " ADEV && \
-		if [ ! -z "$${ADEV}" ] ; then AUDIO="$${ADEV}" ; fi ; \
-		echo "AUDIO=$${AUDIO}" >> /tmp/$$.env && \
-		read -p "Override Latency (ms)? ($${LATENCY_MS}) " LMS && \
-		if [ ! -z "$${LMS}" ] ; then LATENCY_MS=$${LMS} ; fi ; \
-		echo "LATENCY_MS=$${LATENCY_MS}" >> /tmp/$$.env && \
-		echo "PLATFORM=$(PLATFORM)" >> /tmp/$$.env && \
-		$(SUDO) install -Dm600 /tmp/$$.env $@ ; \
-		rm /tmp/$$.env )
 
 clean:
 	/bin/true
 
 dependencies:
-	$(SUDO) apt-get update
-	@PLATFORM=$(PLATFORM) ./ensure-gst.sh --dry-run
-	@PLATFORM=$(PLATFORM) ./ensure-gstd.sh --dry-run
-	$(SUDO) apt-get install -y $(PKGDEPS)
+	@if [ ! -z "$(PKGDEPS)" ] && [ -z "$(DRY_RUN)" ] ; then \
+		$(SUDO) apt-get update ; \
+		$(SUDO) apt-get install -y $(PKGDEPS) ; \
+	fi
+	@PLATFORM=$(PLATFORM) ./ensure-gst.sh $(DRY_RUN)
+	@PLATFORM=$(PLATFORM) ./ensure-gstd.sh $(DRY_RUN)
 	$(MAKE) --no-print-directory $(SPEEDTEST)
 
 disable:
@@ -123,13 +102,35 @@ install: git-cache
 	@for s in $(SERVICES) ; do $(SUDO) systemctl enable $${s%.*} ; done
 
 provision:
-	$(MAKE) --no-print-directory FLAGS=$(FLAGS) -B $(SYSCFG)
-	$(MAKE) --no-print-directory -B /etc/hosts
-	@(	UDP_IFACE=$(shell $(SUDO) grep UDP_IFACE $(SYSCFG) | cut -f2 -d=) && \
-		UDP_IFACE=$$UDP_IFACE $(MAKE) --no-print-directory -B /etc/network/interfaces )
+	# NB: order is important in generating these files
+	$(MAKE) --no-print-directory -B $(SYSCFG)/video-stream.conf $(DRY_RUN)
+	@for s in $(SERVICES) ; do $(MAKE) --no-print-directory -B $(SYSCFG)/$${s%.*}.conf $(DRY_RUN) ; done
+	$(MAKE) --no-print-directory -B $(LOCAL)/bin/camera-switcher.sh $(DRY_RUN)
+	$(MAKE) --no-print-directory -B $(LOCAL)/bin/audio-streamer.sh $(DRY_RUN)
+	$(SUDO) systemctl restart audio-streamer camera-switcher
+
+provision-audio:
+	$(MAKE) --no-print-directory -B $(SYSCFG)/audio-streamer.conf $(DRY_RUN)
+	$(MAKE) --no-print-directory -B $(LOCAL)/bin/audio-streamer.sh $(DRY_RUN)
+	$(SUDO) systemctl restart audio-streamer
+
+provision-cameras:
+	$(MAKE) --no-print-directory -B $(SYSCFG)/camera-switcher.conf $(DRY_RUN)
+	$(MAKE) --no-print-directory -B $(LOCAL)/bin/camera-switcher.sh $(DRY_RUN)
+	$(SUDO) systemctl restart camera-switcher
+
+provision-video:
+	$(MAKE) --no-print-directory -B $(SYSCFG)/video-stream.conf $(DRY_RUN)
+	$(MAKE) --no-print-directory -B $(SYSCFG)/camera-switcher.conf $(DRY_RUN)
+	$(MAKE) --no-print-directory -B $(LOCAL)/bin/camera-switcher.sh $(DRY_RUN)
+	$(SUDO) systemctl restart camera-switcher
+
+show-config:
+	@for s in video-stream.service $(SERVICES) ; do echo "*** $${s%.*}.conf ***" && $(SUDO) cat $(SYSCFG)/$${s%.*}.conf ; done
+	@echo "*** cameras ***" && ls -al /dev/cam*
 
 /etc/hosts: Makefile
-	@(	URL=$(shell $(SUDO) grep URL $(SYSCFG) | cut -f2 -d=) && \
+	@(	URL=$(shell $(SUDO) grep URL $(SYSCFG)/video-stream.conf | cut -f2 -d=) && \
 		if [ ! -z "$${URL}" -a "$${URL}" != "udp" ] ; then \
 			SVR=$$(echo $$URL | cut -f2 -d: | sed -e 's/\/*//') && \
 			read -p "Server for RTMP stream? ($${SVR}) " VS && \
@@ -138,12 +139,6 @@ provision:
 				python3 override.py $$SVR /etc/hosts ; \
 			fi ; \
 		fi )
-
-/etc/network/interfaces: Makefile
-	@(	UDP_ADDR=$(shell ip addr show $${UDP_IFACE} | grep inet | grep -v inet6 | head -1 | sed -e 's/^[[:space:]]*//' | cut -f2 -d' ' | cut -f1 -d/) && \
-		read -p "IPv4 Address for $${UDP_IFACE}? ($${UDP_ADDR}) " UA && \
-		if [ ! -z "$${UA}" ] ; then UDP_ADDR=$${UA} ; fi ; \
-		python3 override.py "$${UDP_ADDR}" $@ )
 
 test:
 	-@( gstd -k && gstd )
@@ -156,7 +151,7 @@ test:
 
 uninstall:
 	-@gstd -k
-	-( cd $(LOCAL)/bin && $(SUDO) rm $(LOCAL_APPS) )
+	-( cd $(LOCAL)/bin && $(SUDO) rm $(LOCAL_APPS) $(LOCAL_SCRIPTS) )
 	@-for c in stop disable ; do $(SUDO) systemctl $${c} $(SERVICES) ; done
 	@for s in $(SERVICES) ; do $(SUDO) rm $(LIBSYSTEMD)/$${s%.*}.service ; done
 	@if [ ! -z "$(SERVICES)" ] ; then $(SUDO) systemctl daemon-reload ; fi
---
+++ provision.sh
@@ -0,0 +1,264 @@
+#!/bin/bash
+# usage:
+#   provision.sh filename [--dry-run]
+#
+# Interactively create/update a systemd service configuration file
+
+SUDO=$(test ${EUID} -ne 0 && which sudo)
+SYSCFG=/etc/systemd
+UDEV_RULESD=/etc/udev/rules.d
+
+CONF=$1
+shift
+DEFAULTS=false
+DRY_RUN=false
+while (($#)) ; do
+	if [ "$1" == "--dry-run" ] && ! $DRY_RUN ; then DRY_RUN=true ; set -x ;
+	elif [ "$1" == "--defaults" ] ; then DEFAULTS=true ;
+	fi
+	shift
+done
+
+function address_of {
+	local result=$(ip addr show $1 | grep inet | grep -v inet6 | head -1 | sed -e 's/^[[:space:]]*//' | cut -f2 -d' ' | cut -f1 -d/)
+	echo $result
+}
+
+function value_of {
+	local result=$($SUDO grep $1 $CONF 2>/dev/null | cut -f2 -d=)
+	if [ -z "$result" ] ; then result=$2 ; fi
+	echo $result
+}
+
+# pull default provisioning items from the network.conf (generate it first)
+function value_from_network {
+	local result=$($SUDO grep $1 $(dirname $CONF)/network.conf 2>/dev/null | cut -f2 -d=)
+	if [ -z "$result" ] ; then result=$2 ; fi
+	echo $result
+}
+
+function interactive {
+	local result
+	read -p "${2}? ($1) " result
+	if [ -z "$result" ] ; then result=$1 ; elif [ "$result" == "*" ] ; then result="" ; fi
+	echo $result
+}
+
+function contains {
+	local result=no
+	#if [[ " $2 " =~ " $1 " ]] ; then result=yes ; fi
+	if [[ $2 == *"$1"* ]] ; then result=yes ; fi
+	echo $result
+}
+
+# configuration values used in this script
+declare -A config
+config[iface]=$(value_from_network IFACE wlan0)
+
+case "$(basename $CONF)" in
+
+	camera-switcher.conf)
+		config[cam1]=$(value_of CAM1 "")
+		config[cam2]=$(value_of CAM2 "")
+		config[cam3]=$(value_of CAM3 "")
+		if ! $DEFAULTS ; then
+			echo ""
+			for d in /dev/video[0-9]* ; do
+				echo "*** $d ***"
+				if udevadm info -a -n $d | grep ATTRS | grep -E 'manufacturer|product|devpath' | head -3 ; then
+					v4l2-ctl -d $d --list-formats
+					echo ""
+				fi
+			done
+			config[cam1]=$(interactive "${config[cam1]}" "Select device number (/dev/video*) for camera 1")
+			config[cam2]=$(interactive "${config[cam2]}" "Select device number (/dev/video*) for camera 2 (x disables)")
+			config[cam3]=$(interactive "${config[cam3]}" "Select device number (/dev/video*) for camera 3 (x disables)")
+		fi
+		# generate udev rules for selected cameras
+		touch /tmp/$$.rule
+		for n in 1 2 3 ; do
+			# https://wiki.archlinux.org/index.php/Udev#Video_device
+			if [ "${config[cam${n}]}" == "x" ] ; then
+				echo "*** cam${n} skipped ***"
+			elif [ ! -z "${config[cam${n}]}" ] ; then
+				ok=true
+				udevadm info -a -n /dev/video${config[cam${n}]} | grep ATTR > /tmp/camera${n}.$$
+				for kw in devpath idProduct idVendor index ; do
+					config[$kw]=$(grep $kw /tmp/camera${n}.$$ | head -1 | cut -f2 -d\")
+					if [ -z "${config[$kw]}" ] ; then ok=false ; fi
+				done
+				if $ok ; then
+					echo "SUBSYSTEM==\"video4linux\", ATTRS{idVendor}==\"${config[idVendor]}\", ATTRS{idProduct}==\"${config[idProduct]}\", ATTRS{devpath}==\"${config[devpath]}\", ATTR{index}==\"${config[index]}\", SYMLINK+=\"cam${n}\"" >> /tmp/$$.rule
+				else
+					echo "*** /dev/video${config[cam${n}]} not configured for cam${n} ***"
+				fi
+			fi
+		done
+		if $DRY_RUN ; then
+			echo ${UDEV_RULESD}/83-webcam.rules && cat /tmp/$$.rule && echo ""
+		else
+			set -x
+			$SUDO install -Dm644 /tmp/$$.rule ${UDEV_RULESD}/83-webcam.rules
+			$SUDO udevadm control --reload-rules && $SUDO udevadm trigger
+			set +x
+		fi
+		echo "[Service]" > /tmp/$$.env && \
+		echo "CAM1=${config[cam1]}" >> /tmp/$$.env && \
+		echo "CAM2=${config[cam2]}" >> /tmp/$$.env && \
+		echo "CAM3=${config[cam3]}" >> /tmp/$$.env && \
+		echo "CONF=$(dirname $CONF)/video-stream.conf" >> /tmp/$$.env
+		;;
+
+	camera-switcher.sh)
+		# https://unix.stackexchange.com/questions/79068/how-to-export-variables-that-are-set-all-at-once
+		# set -a && source /etc/systemd/video-stream.conf && set +a
+		x=$(tail -n +2 /etc/systemd/video-stream.conf) && set -a && eval $x && set +a
+		# now we have the environment settings for video
+		p1=$(PLATFORM=${PLATFORM} VIDEO_DEVICE=/dev/cam1 FLAGS=debug,smpte,${FLAGS} ./video-stream.sh 2>/dev/null)
+		p2=$(PLATFORM=${PLATFORM} VIDEO_DEVICE=/dev/cam2 FLAGS=debug,smpte,${FLAGS} ./video-stream.sh 2>/dev/null)
+		p3=$(PLATFORM=${PLATFORM} VIDEO_DEVICE=/dev/cam3 FLAGS=debug,smpte,${FLAGS} ./video-stream.sh 2>/dev/null)
+		if [ -z "$p1" ] ; then echo "*** Did not produce cam1 pipeline - STOP" ; exit 1 ; fi
+		if [ -z "$p2" ] ; then echo "*** Did not produce cam2 pipeline - STOP" ; exit 1 ; fi
+		if [ -z "$p3" ] ; then echo "*** Did not produce cam3 pipeline - STOP" ; exit 1 ; fi
+		# now we have the 3 pipelines that should be executed
+		echo "#!/bin/bash" > /tmp/$$.env && \
+		echo "# ensure previous pipelines are cancelled and cleared" >> /tmp/$$.env && \
+		echo "gstd -f /var/run -l /dev/null -d /dev/null -k" >> /tmp/$$.env && \
+		echo "gstd -f /var/run -l /var/run/camera-switcher/gstd.log -d /var/run/camera-switcher/gst.log" >> /tmp/$$.env && \
+		echo "gst-client pipeline_create cam1 $p1" >> /tmp/$$.env && \
+		echo "gst-client pipeline_create cam2 $p2" >> /tmp/$$.env && \
+		echo "gst-client pipeline_create cam3 $p3" >> /tmp/$$.env && \
+		echo "# start cam1 by default" >> /tmp/$$.env && \
+		echo "gst-client pipeline_play cam1" >> /tmp/$$.env && \
+		echo "" >> /tmp/$$.env
+		;;
+
+	audio-streamer.conf)
+		IFACE=$(value_of IFACE ${config[iface]})
+		HOST=$(value_of HOST 224.1.$(echo $(address_of ${IFACE}) | cut -f3,4 -d.))
+		_NAME=$(value_of NAME "") ; NAME=${_NAME//\"}
+		PORT=$(value_of PORT 5601)
+		if ! $DEFAULTS ; then
+			arecord -l | grep 'card.*device'
+			NAME=$(interactive "$NAME" "Audio device name")
+			IFACE=$(interactive "$IFACE" "UDP Interface for audio")
+			HOST=$(interactive "$HOST" "UDP IPv4 for audio")
+			PORT=$(interactive "$PORT" "UDP PORT for audio")
+		fi
+		echo "[Service]" > /tmp/$$.env && \
+		echo "IFACE=${IFACE}" >> /tmp/$$.env && \
+		echo "HOST=${HOST}" >> /tmp/$$.env && \
+		echo "NAME=\"${NAME//\"}\"" >> /tmp/$$.env && \
+		echo "PORT=${PORT}" >> /tmp/$$.env
+		;;
+
+	audio-streamer.sh)
+		# https://unix.stackexchange.com/questions/79068/how-to-export-variables-that-are-set-all-at-once
+		x=$(tail -n +2 /etc/systemd/audio-streamer.conf) && set -a && eval $x && set +a
+		# now we have the environment settings for audio
+		p1=$(PLATFORM=${PLATFORM} IFACE=${IFACE} HOST=${HOST} NAME=\"${NAME}\" PORT=${PORT} DEBUG=true ./audio-stream.sh 2>/dev/null)
+		if [ -z "$p1" ] ; then echo "*** Did not produce audio pipeline - STOP" ; exit 1 ; fi
+		echo "#!/bin/bash" > /tmp/$$.env && \
+		echo "gst-launch-1.0 $p1" >> /tmp/$$.env && \
+		echo "" >> /tmp/$$.env
+		;;
+
+	@(	SN=$(shell python3 serial_number.py) && \
+		URL=$(shell $(SUDO) grep URL $(SYSCFG) | cut -f2 -d=) && \
+		SKEY=$(shell $(SUDO) grep SKEY $(SYSCFG) | cut -f2 -d=) && \
+		AUDIO=$(shell $(SUDO) grep AUDIO $(SYSCFG) | cut -f2 -d=) && \
+		LATENCY_MS=$(shell $(SUDO) grep LATENCY_MS $(SYSCFG) | cut -f2 -d=) && \
+		read -p "udp or URL for video server? ($${URL}) " UR && \
+		if [ ! -z "$${UR}" ] ; then URL=$${UR} ; fi ; \
+		if [ -z "$${SKEY}}" ] ; then SKEY=$${SN} ; fi ; \
+		read -p "Stream Key? ($${SKEY}) " SK && \
+		if [ ! -z "$${SK}" ] ; then SKEY=$${SK} ; fi ; \
+		echo "[Service]" > /tmp/$$.env ; \
+		x=$$(echo $$URL | grep mavnet.online) && \
+		if [ ! -z "$$x" ] ; then \
+			USERNAME=$(shell $(SUDO) grep USERNAME $(SYSCFG) | cut -f2 -d=) && \
+			read -p "Username for video server? ($${USERNAME}) " UN && \
+			if [ ! -z "$${UN}" ] ; then USERNAME=$${UN} ; fi ; \
+			read -s -p "Password? " KEY ; echo "" ; \
+			echo "KEY=$${KEY}" >> /tmp/$$.env && \
+			echo "USERNAME=$${USERNAME}" >> /tmp/$$.env && \
+			echo "SERVER=$${SERVER}" >> /tmp/$$.env && \
+			SKEY="$${SN}" && \
+			URL="rtmp://$${SERVER}:$${SERVER_PORT}/$${SERVER_GROUP}" ; \
+		fi ; \
+		if [ "$$URL" == "udp" ] ; then \
+			FLAGS="audio,h264,mjpg,udp,xraw"
+			UDP_IFACE=$(shell $(SUDO) grep UDP_IFACE $(SYSCFG) | cut -f2 -d=) && \
+			UDP_HOST=$(shell $(SUDO) grep UDP_HOST $(SYSCFG) | cut -f2 -d=) && \
+			UDP_PORT=$(shell $(SUDO) grep UDP_PORT $(SYSCFG) | cut -f2 -d=) && \
+			read -p "Network interface device for udp ($${UDP_IFACE}) " UIF && \
+			if [ ! -z "$${UIF}" ] ; then UDP_IFACE=$${UIF} ; fi ; \
+			read -p "UDP port for video stream? ($${UDP_PORT}) " UP && \
+			if [ ! -z "$${UP}" ] ; then UDP_PORT=$${UP} ; fi ; \
+			read -p "UDP address for video stream? ($${UDP_HOST}) " UH && \
+			if [ ! -z "$${UH}" ] ; then UDP_HOST=$${UH} ; fi ; \
+			echo "UDP_IFACE=$${UDP_IFACE}" >> /tmp/$$.env && \
+			echo "UDP_HOST=$${UDP_HOST}" >> /tmp/$$.env && \
+			echo "UDP_PORT=$${UDP_PORT}" >> /tmp/$$.env ; \
+		fi ; \
+		echo "URL=$${URL}" >> /tmp/$$.env && \
+		echo "SKEY=$${SKEY}" >> /tmp/$$.env && \
+		echo "FLAGS=$(FLAGS)" >> /tmp/$$.env && \
+		read -p "Audio Device? ($${AUDIO}) " ADEV && \
+		if [ ! -z "$${ADEV}" ] ; then AUDIO="$${ADEV}" ; fi ; \
+		echo "AUDIO=$${AUDIO}" >> /tmp/$$.env && \
+		read -p "Override Latency (ms)? ($${LATENCY_MS}) " LMS && \
+		if [ ! -z "$${LMS}" ] ; then LATENCY_MS=$${LMS} ; fi ; \
+		echo "LATENCY_MS=$${LATENCY_MS}" >> /tmp/$$.env && \
+		echo "PLATFORM=$(PLATFORM)" >> /tmp/$$.env && \
+		$(SUDO) install -Dm600 /tmp/$$.env $@ ; \
+		rm /tmp/$$.env )
+
+
+	video-stream.conf)
+		# special case of provisioning the single camera streamer
+		UDP_IFACE=$(value_of UDP_IFACE eth0)
+		UDP_HOST=$(value_of UDP_HOST 224.1.$(echo $(address_of ${UDP_IFACE}) | cut -f3,4 -d.))
+		UDP_PORT=$(value_of UDP_PORT 5600)
+		WIDTH=$(value_of WIDTH 1280)
+		HEIGHT=$(value_of HEIGHT 720)
+		FPS=$(value_of FPS 30)
+		VIDEO_BITRATE=$(value_of VIDEO_BITRATE 1800)
+		FLAGS=$(value_of FLAGS "h264,xraw,scale,udp")
+		URL=$(value_of URL udp)
+		if ! $DEFAULTS ; then
+			UDP_HOST=$(interactive "$UDP_HOST" "RJ45 Network IPv4 destination for video")
+			WIDTH=$(interactive "$WIDTH" "Video stream width")
+			HEIGHT=$(interactive "$HEIGHT" "Video stream height")
+			FPS=$(interactive "$FPS" "Video stream frames/sec")
+			VIDEO_BITRATE=$(interactive "$VIDEO_BITRATE" "Video stream bitrate in kbps/sec")
+			FLAGS=$(interactive "$FLAGS" "Video stream flags")
+		fi
+		echo "[Service]" > /tmp/$$.env && \
+		echo "PLATFORM=${PLATFORM}" >> /tmp/$$.env && \
+		echo "UDP_IFACE=${UDP_IFACE}" >> /tmp/$$.env && \
+		echo "UDP_HOST=${UDP_HOST}" >> /tmp/$$.env && \
+		echo "UDP_PORT=${UDP_PORT}" >> /tmp/$$.env && \
+		echo "WIDTH=${WIDTH}" >> /tmp/$$.env && \
+		echo "HEIGHT=${HEIGHT}" >> /tmp/$$.env && \
+		echo "FPS=${FPS}" >> /tmp/$$.env && \
+		echo "VIDEO_BITRATE=${VIDEO_BITRATE}" >> /tmp/$$.env && \
+		echo "FLAGS=${FLAGS}" >> /tmp/$$.env && \
+		echo "URL=${URL}" >> /tmp/$$.env
+		;;
+
+	*)
+		# preserve contents or generate a viable empty configuration
+		echo "[Service]" > /tmp/$$.env
+		;;
+esac
+
+if $DRY_RUN ; then
+	set +x
+	echo $CONF && cat /tmp/$$.env && echo ""
+elif [[ $(basename $CONF) == *.sh ]] ; then
+	$SUDO install -Dm755 /tmp/$$.env $CONF
+else
+	$SUDO install -Dm644 /tmp/$$.env $CONF
+fi
+rm /tmp/$$.env
