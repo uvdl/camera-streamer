5ff49d245ee186428b84e3345d1638877d701187
UVDL Developer <uvdl@ornl.gov>
2020-09-13 14:28:07 +0000

    as found on NC-5

--- video-client-gst.sh
+++ video-client-gst.sh
@@ -30,6 +30,7 @@ config[video_port]="$1" ; if [ -z "${config[video_port]}" ] ; then config[video_
 config[audio_port]="$2" ; if [ -z "${config[audio_port]}" ] ; then config[audio_port]=0 ; fi
 config[udp_ip]="$3"
 config[mcast_if]="$4" ; if [ -z "${config[mcast_if]}" ] ; then config[mcast_if]=eth0 ; fi
+config[videosink]="$5" ; if [ -z "${config[videosink]}" ] ; then config[videosink]=autovideosink ; fi
 config[video_caps]="application/x-rtp,media=(string)video,clock-rate=(int)90000,encoding-name=(string)H264,payload=(int)96"
 config[audio_caps]="application/x-rtp"
 
@@ -49,7 +50,7 @@ fi
 if [ ${config[video_port]/.*} -eq 0 -a ${config[audio_port]/.*} -eq 0 ] ; then
 	# run test pattern generation to test installation
 	set -x
-	gst-launch-1.0 videotestsrc is-live=true ! "video/x-raw,format=(string)I420,width=(int)640,height=(int)360,framerate=30/1" ! videoconvert ! autovideosink
+	gst-launch-1.0 videotestsrc is-live=true ! "video/x-raw,format=(string)I420,width=(int)640,height=(int)360,framerate=30/1" ! videoconvert ! ${config[videosink]}
 elif [ ${config[video_port]/.*} -eq 0 ] ; then
 	# audio only
 	set -x
@@ -57,19 +58,19 @@ elif [ ${config[video_port]/.*} -eq 0 ] ; then
 elif [ ${config[audio_port]/.*} -eq 0 ] ; then
 	# video only
 	set -x
-	gst-launch-1.0 ${gst[udpsrc]} port=${config[video_port]} caps="${config[video_caps]}" ! rtph264depay ! h264parse ! queue ! decodebin ! progressreport ! autovideosink
+	gst-launch-1.0 ${gst[udpsrc]} port=${config[video_port]} caps="${config[video_caps]}" ! rtph264depay ! h264parse ! queue ! decodebin ! progressreport ! ${config[videosink]}
 elif [ ${config[audio_port]/.*} -eq ${config[video_port]/.*} ] ; then
 	# video+audio using same port (flvmux)
 	set -x
-	gst-launch-1.0 ${gst[udpsrc]} port=${config[video_port]} ! queue ! flvdemux name=mux mux.video ! "${config[video_caps]}" ! rtph264depay ! h264parse ! queue ! decodebin ! autovideosink mux.audio ! "${config[audio_caps]}" ! rtpmp4adepay ! "audio/mpeg,codec_data=(buffer)1290" ! queue ! decodebin ! audioconvert ! autoaudiosink
+	gst-launch-1.0 ${gst[udpsrc]} port=${config[video_port]} ! queue ! flvdemux name=mux mux.video ! "${config[video_caps]}" ! rtph264depay ! h264parse ! queue ! decodebin ! ${config[videosink]} mux.audio ! "${config[audio_caps]}" ! rtpmp4adepay ! "audio/mpeg,codec_data=(buffer)1290" ! queue ! decodebin ! audioconvert ! autoaudiosink
 elif ${config[async]} ; then
 	# video+audio using separate ports and separate processes
 	# https://unix.stackexchange.com/questions/204480/run-multiple-commands-and-kill-them-as-one-in-bash
 	# https://stackoverflow.com/questions/3004811/how-do-you-run-multiple-programs-in-parallel-from-a-bash-script
 	set -x
-	gst-launch-1.0 ${gst[udpsrc]} port=${config[video_port]} caps="${config[video_caps]}" ! rtph264depay ! h264parse ! queue ! decodebin ! autovideosink & gst-launch-1.0 ${gst[udpsrc]} port=${config[audio_port]} caps="${config[audio_caps]}" ! rtpmp4adepay ! "audio/mpeg,codec_data=(buffer)1290" ! queue ! decodebin ! audioconvert ! autoaudiosink && fg
+	gst-launch-1.0 ${gst[udpsrc]} port=${config[video_port]} caps="${config[video_caps]}" ! rtph264depay ! h264parse ! queue ! decodebin ! ${config[videosink]} & gst-launch-1.0 ${gst[udpsrc]} port=${config[audio_port]} caps="${config[audio_caps]}" ! rtpmp4adepay ! "audio/mpeg,codec_data=(buffer)1290" ! queue ! decodebin ! audioconvert ! autoaudiosink && fg
 else
 	# video+audio using separate ports and the same process (forces sync)
 	set -x
-	gst-launch-1.0 ${gst[udpsrc]} port=${config[video_port]} caps="${config[video_caps]}" ! rtph264depay ! h264parse ! queue ! decodebin ! autovideosink ${gst[udpsrc]} port=${config[audio_port]} caps="${config[audio_caps]}" ! rtpmp4adepay ! "audio/mpeg,codec_data=(buffer)1290" ! queue ! decodebin ! audioconvert ! autoaudiosink
+	gst-launch-1.0 ${gst[udpsrc]} port=${config[video_port]} caps="${config[video_caps]}" ! rtph264depay ! h264parse ! queue ! decodebin ! ${config[videosink]} ${gst[udpsrc]} port=${config[audio_port]} caps="${config[audio_caps]}" ! rtpmp4adepay ! "audio/mpeg,codec_data=(buffer)1290" ! queue ! decodebin ! audioconvert ! autoaudiosink
 fi
